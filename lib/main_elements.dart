625e2b2095eccdd25357e9365c019eecede953a8
SV <simona.vel719@gmail.com>
2026-09-06 17:02:57 +0300

    Buttons for types of emergencies added (front-end)

---
+++ lib/main_elements.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:emergency_app/parameters.dart';
+
+class EmergencyButton extends StatelessWidget {
+  const EmergencyButton({
+    super.key,
+    required this.onPressed,
+    required this.id,
+    required this.text,
+  });
+
+  final String id;
+  final String text;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      key: Key(id),
+      color: Theme.of(context).colorScheme.inversePrimary,
+      alignment: Alignment.center,
+      margin: EdgeInsets.all(Parameters.buttonPadding),
+      child: TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          minimumSize: Size(
+            double.infinity,
+            double.infinity,
+          ),
+        ),
+        child: Text(
+          text,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/parameters.dart
@@ -0,0 +1,11 @@
+class Parameters {
+  static double buttonPadding = 3.0;
+  static Map<String, String> emergencyTypes = {
+    'medical_emergency_button' : 'Medical emergency',
+    'natural_disaster_button' : 'Natural disaster',
+    'abuse_button' : 'Abuse',
+    'minor_missing_button' : 'Minor missing',
+    'catastrophic_event_button' : 'Catastrophic event',
+    'other_button' : 'Other',
+  };
+}
